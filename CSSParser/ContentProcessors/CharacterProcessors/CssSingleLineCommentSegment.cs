9f812cb2c0a0a695011a9ce4a279db63c75b5620
Dan Roberts <dan@newmind.co.uk>
2013-01-30 08:01:38 +0000

    Tweak to CssSingleLineCommentSegment to consider the terminating line return to be part of the comment content, to be consistent with the MultiLineCommentSegment which (correctly) considers the "*/" content to be part of the comment

--- CSSParser/ContentProcessors/CharacterProcessors/CssSingleLineCommentSegment.cs
+++ CSSParser/ContentProcessors/CharacterProcessors/CssSingleLineCommentSegment.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using CSSParser.StringNavigators;
+using CSSParser.ContentProcessors.CharacterProcessors.Factories;
 
 namespace CSSParser.ContentProcessors.CharacterProcessors
 {
 	public class CssSingleLineCommentSegment : CommentSegment
 	{
-		public CssSingleLineCommentSegment(IProcessCharacters characterProcessorToReturnTo) : base(characterProcessorToReturnTo) { }
+		private readonly IGenerateCharacterProcessors _processorFactory;
+		public CssSingleLineCommentSegment(IProcessCharacters characterProcessorToReturnTo, IGenerateCharacterProcessors processorFactory) : base(characterProcessorToReturnTo)
+		{
+			if (processorFactory == null)
+				throw new ArgumentNullException("processorFactory");
+
+			_processorFactory = processorFactory;
+		}
 
 		public override CharacterProcessorResult Process(IWalkThroughStrings stringNavigator)
 		{
 			if (stringNavigator == null)
 				throw new ArgumentNullException("stringNavigator");
 
-			if ((stringNavigator.CurrentCharacter == '\r') || (stringNavigator.CurrentCharacter == '\n'))
+			// For single line comments, the line return should be considered part of the comment content (in the same way that the "/*" and "*/" sequences are
+			// considered part of the content for multi-line comments)
+			if (stringNavigator.TryToGetCharacterString(2) == "\r\n")
+			{
+				return new CharacterProcessorResult(
+					CharacterCategorisationOptions.Comment,
+					_processorFactory.Get<SkipCharactersSegment>(
+						CharacterCategorisationOptions.Comment,
+						1,
+						_characterProcessorToReturnTo
+					)
+				);
+			}
+			else if ((stringNavigator.CurrentCharacter == '\r') || (stringNavigator.CurrentCharacter == '\n'))
 				return new CharacterProcessorResult(CharacterCategorisationOptions.Comment, _characterProcessorToReturnTo);
 
 			return new CharacterProcessorResult(CharacterCategorisationOptions.Comment, this);
