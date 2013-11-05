fc2dbc42f07704dc15272c66cc606fbb49bb4bc5
dan@newmind.co.uk <dan@newmind.co.uk>
2013-11-05 20:24:14 +0000

    Fixed up solution and projects to get rid of the "processor target" warning when building against the CSSMinifier against "Any CPU" and referencing the CSSParser

--- CSSParser.ExtendedLESSParser/Properties/AssemblyInfo.cs
+++ CSSParser.ExtendedLESSParser/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExtendedLESSParser")]
+[assembly: AssemblyTitle("CSSParser.ExtendedLESSParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExtendedLESSParser")]
+[assembly: AssemblyProduct("CSSParser.ExtendedLESSParser")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
